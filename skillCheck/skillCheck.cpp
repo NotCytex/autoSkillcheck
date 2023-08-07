79cbcf08d925b63e0be418614b04e12583b2fdb0
Cysex <93426138+Cysex@users.noreply.github.com>
2023-08-08 00:18:43 +1000

    changed detectSpace to cropSpace

--- skillCheck/skillCheck.cpp
+++ skillCheck/skillCheck.cpp
@@ -19,10 +19,9 @@ screencapture sc;
 
 
 cv::Mat space = cv::imread("./resources/space.png"); // Load the template image
-bool detectSpace(cv::Mat& inputImage) {
-	cv::Mat grayInput;
+cv::Mat cropSpace(cv::Mat& inputImage) {
+	cv::Mat grayInput, graySpace;
 	cv::cvtColor(inputImage, grayInput, cv::COLOR_BGR2GRAY); // Convert the input image to grayscale
-	cv::Mat graySpace;
 	cv::cvtColor(space, graySpace, cv::COLOR_BGR2GRAY); // Convert the space image to grayscale
 
 	cv::Mat result;
@@ -33,58 +32,66 @@ bool detectSpace(cv::Mat& inputImage) {
 	cv::minMaxLoc(result, &minVal, &maxVal, &minLoc, &maxLoc); // Find the location with maximum similarity
 
 	if (maxVal > 0.8) { // If the match is stronger than 80%
-		return true;
+		int padding = 90;
+		cv::Point topLeft(max(minLoc.x - padding, 0), max(minLoc.y - padding, 0));
+		cv::Point bottomRight(min(minLoc.x + space.cols + padding, inputImage.cols),
+			min(minLoc.y + space.rows + padding, inputImage.rows));
+
+		// Create and return the cropped image
+		cv::Rect roi(topLeft, bottomRight);
+		cv::Mat croppedImage = inputImage(roi).clone();
+		return croppedImage;
 	}
 
-	return false;
+	return cv::Mat();
 }
 
 
-//cv::Scalar whiteLow = cv::Scalar(184, 185, 185);
-//cv::Scalar whiteHigh = cv::Scalar(193, 188, 188);
-//cv::Mat extractWhiteBoxContour(const cv::Mat& inputImage) {
-//	cv::Mat background;
-//	inputImage.copyTo(background);
-//
-//	cv::cvtColor(inputImage, inputImage, cv::COLOR_BGR2RGB);
-//
-//	cv::Mat mask;
-//	cv::inRange(inputImage, whiteLow, whiteHigh, mask);
-//	cv::imshow("mask", mask);
-//
-//	std::vector<std::vector<cv::Point>> contours;
-//	cv::findContours(mask, contours, cv::RETR_LIST, cv::CHAIN_APPROX_SIMPLE);
-//
-//	std::vector<cv::Point> whiteBoxContour;
-//	for (const auto& contour : contours) {
-//		double contourArea = cv::contourArea(contour);
-//		cv::Rect boundingRect = cv::boundingRect(contour);
-//		double aspectRatio = static_cast<double>(boundingRect.width) / boundingRect.height;
-//
-//		// Criteria to filter the white box contour
-//		if (contourArea > 5 && contourArea < 200 &&
-//			boundingRect.width > 5 && boundingRect.height > 5 &&
-//			boundingRect.width < 200 && boundingRect.height < 200) {
-//
-//			std::cout << contourArea << std::endl;
-//			std::cout << boundingRect.width << std::endl;
-//			std::cout << boundingRect.height << std::endl;
-//
-//			whiteBoxContour = contour;
-//			break; // Assuming there is only one white box, exit the loop after finding it
-//		}
-//	}
-//
-//	cv::Mat outputImage = inputImage.clone();
-//	if (!whiteBoxContour.empty()) {
-//		cv::drawContours(outputImage, std::vector<std::vector<cv::Point>>{whiteBoxContour}, -1, cv::Scalar(0, 255, 0), 2);
-//	}
-//	else {
-//		std::cout << "None" << std::endl;
-//	}
-//
-//	return outputImage;
-//}
+cv::Scalar whiteLow = cv::Scalar(184, 185, 185);
+cv::Scalar whiteHigh = cv::Scalar(193, 191, 190);
+cv::Mat extractWhiteBoxContour(const cv::Mat& inputImage) {
+	cv::Mat background;
+	inputImage.copyTo(background);
+
+	cv::cvtColor(inputImage, inputImage, cv::COLOR_BGR2RGB);
+
+	cv::Mat mask;
+	cv::inRange(inputImage, whiteLow, whiteHigh, mask);
+	cv::imshow("mask", mask);
+
+	std::vector<std::vector<cv::Point>> contours;
+	cv::findContours(mask, contours, cv::RETR_LIST, cv::CHAIN_APPROX_SIMPLE);
+
+	std::vector<cv::Point> whiteBoxContour;
+	for (const auto& contour : contours) {
+		double contourArea = cv::contourArea(contour);
+		cv::Rect boundingRect = cv::boundingRect(contour);
+		double aspectRatio = static_cast<double>(boundingRect.width) / boundingRect.height;
+
+		// Criteria to filter the white box contour
+		if (contourArea > 2 && contourArea < 200 &&
+			boundingRect.width > 2 && boundingRect.height > 2 &&
+			boundingRect.width < 200 && boundingRect.height < 200) {
+
+			std::cout << contourArea << std::endl;
+			std::cout << boundingRect.width << std::endl;
+			std::cout << boundingRect.height << std::endl;
+
+			whiteBoxContour = contour;
+			break; // Assuming there is only one white box, exit the loop after finding it
+		}
+	}
+
+	cv::Mat outputImage = inputImage.clone();
+	if (!whiteBoxContour.empty()) {
+		cv::drawContours(outputImage, std::vector<std::vector<cv::Point>>{whiteBoxContour}, -1, cv::Scalar(0, 255, 0), 2);
+	}
+	else {
+		std::cout << "None" << std::endl;
+	}
+
+	return outputImage;
+}
 	
 
 int main() {
@@ -95,7 +102,7 @@ int main() {
 	//	std::cerr << "Could not find window. Error: " << GetLastError() << std::endl;
 	//	return -1;
 	//}
-	//cv::namedWindow("output", cv::WINDOW_NORMAL);
+	cv::namedWindow("output", cv::WINDOW_NORMAL);
 
 	//// Declare variables to calculate FPS
 	//double fps;
@@ -122,13 +129,12 @@ int main() {
 	//}
 	
 	
-	cv::Mat test = cv::imread("./resources/nig4.png");
-	//cv::Mat cropped = cropSpace(test);
-	std::cout << detectSpace(test) << std::endl;
-	//cv::Mat outputImage = extractWhiteBoxContour(test);
+	cv::Mat test = cv::imread("./resources/nig2.png");
+	cv::Mat cropped = cropSpace(test);
+	cv::Mat outputImage = extractWhiteBoxContour(test);
 	
-	//cv::imshow("cropped", cropped);
-	//cv::imshow("output", outputImage);
+	cv::imshow("cropped", cropped);
+	cv::imshow("output", outputImage);
 
 	cv::waitKey(0);
 	return 0;
